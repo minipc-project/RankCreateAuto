2e1acb8eb1da3b2f2d226dcf0dd6e7c330781fbc
coderok <myxi2002@gmail.com>
2023-12-31 18:38:36 +0500

    Hot fix for GitAction

--- RankCreate.py
+++ RankCreate.py
@@ -1,33 +1,44 @@
+import yaml
 import math
 
 def calculate_growth_rate(x1, y1, x2, y2):
-    # Calculate the growth rate b given two points (x1, y1) and (x2, y2) on the curve
-    return ((y2/y1) ** (1/(x2-x1)))
+    # Calculates the exponential growth rate given two points
+    return (y2/y1)**(1/(x2-x1))
 
-def create_ranks_file(filename):
-    initial_cost = 20  # Cost at rank 1
-    final_cost = 700000  # Cost at rank 61
-    initial_rank = 1  # Initial rank
-    final_rank = 61  # Final rank where final_cost is known
-    
-    # Calculate the growth rate
+def calculate_cost(a, b, rank):
+    # Special case for rank 0
+    if rank == 0:
+        return 0
+    return int(round(a * (b ** (rank - 1))))
+
+def generate_ranks_data(initial_cost, final_cost, initial_rank, final_rank):
+    # Calculate growth rate 'b' using the initial and final known costs
     b = calculate_growth_rate(initial_rank, initial_cost, final_rank, final_cost)
+    
+    ranks_data = {}
+    for rank in range(0, 131):  # Including rank 0
+        if rank == 0:
+            ranks_data[str(rank)] = {'Prefix': '&a&l0'}   # Only Prefix for rank 0
+        else:
+            # For other ranks, include command with rank number as well
+            command = f"lp user %player% perm set cmi.command.warp.a{rank}"
+            ranks_data[str(rank)] = {
+                'Prefix': f"&a&l{rank}",
+                'Cost': calculate_cost(initial_cost, b, rank),
+                'CMD': [command]
+            }
+    
+    return ranks_data
 
+def create_ranks_file(filename, ranks_data):
     with open(filename, 'w') as file:
-        file.write("Ranks:\n")
-        
-        # Iterate through the ranks and write their data
-        for rank in range(1, 131):  # Ranks from 1 to 130
-            file.write(f"  '{rank}':\n")          # Rank ID
-            file.write(f"    Prefix: '&a&l{rank}'\n")  # Rank prefix
-            if rank == 1:
-                cost = initial_cost  # The cost for rank 1 is defined as the initial cost
-            else:
-                # Calculate the cost for every other rank
-                cost = initial_cost * (b ** (rank - 1))
-            file.write(f"    Cost: {int(round(cost))}\n")  # Write the cost rounded to an integer
-            file.write("    CMD:\n")                       # Commands placeholder
-            file.write(f"    - lp user %player% perm set cmi.command.warp.mine{rank}\n")  # Example command
-            
-# Replace 'ranks.yml' with the path to the file you want to generate
-create_ranks_file('ranks.yml')
+        yaml.dump({'Ranks': ranks_data}, file, default_flow_style=False)
+
+# Main execution
+initial_cost = 20
+final_cost = 700000
+initial_rank = 1
+final_rank = 61
+
+ranks_data = generate_ranks_data(initial_cost, final_cost, initial_rank, final_rank)
+create_ranks_file('ranks.yml', ranks_data)
